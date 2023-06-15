e4226bd5de2dd308178c931124ad3d83a970eb84
summer <saunogq2@naver.com>
2023-06-15 18:06:47 +0900

    게시판 서브페이지 이전 서브페이지 완료

--- index.js
+++ index.js
@@ -116,9 +116,186 @@ app.get("/allprogram",function(req,res){
   res.render("allprogram.ejs")
 })
 
+app.get("/allprogram/detail/:num",function(req,res){
+  
+  const Num = Number(req.params.num);
+  const menuList = [
+    "1夜. 야경(夜景)",
+    "2夜. 야로(夜路)",
+    "3夜. 야사(夜史)",
+    "4夜. 야설(夜說)",
+    "5夜. 야식(夜食)",
+    "6夜. 야시(夜市)",
+    "7夜. 야화(夜畵)",
+    "8夜. 야숙(夜宿)",
+    "9夜. 야주(夜晝)",
+  ]
 
-app.get("/subHeader",function(req,res){
-  res.render("subHeader.ejs")
+  res.render("allprogram_detail.ejs", { Num: Num , menuList:menuList});
+}) 
+
+app.get("/sub1",function(req,res){
+  const menuList = [
+    "1夜. 야경(夜景)",
+    "2夜. 야로(夜路)",
+    "3夜. 야사(夜史)",
+    "4夜. 야설(夜說)",
+    "5夜. 야식(夜食)",
+    "6夜. 야시(夜市)",
+    "7夜. 야화(夜畵)",
+    "8夜. 야숙(夜宿)",
+    "9夜. 야주(夜晝)",
+  ]
+  const listData = [
+    {
+      subtitle:"사비풍류",
+      subtitle2:"사비만의 소박한빛"
+    },
+    {
+      subtitle:"사비단길",
+      subtitle2:"사비로운 단아한 길"
+    },
+    {
+      subtitle:"사비만담",
+      subtitle2:"못다한 사비백제 이야기"
+    },
+    {
+      subtitle:"사비풍류",
+      subtitle2:"사비로운 예술공연"
+    },
+    {
+      subtitle:"사비달콤",
+      subtitle2:"사비와 미식거리"
+    },
+    {
+      subtitle:"사비마당",
+      subtitle2:"시끌벅적 사비장터"
+    },
+    {
+      subtitle:"사비미미",
+      subtitle2:"사비로운 예술작품"
+    },
+    {
+      subtitle:"사비하루",
+      subtitle2:"사비에서의 하룻밤"
+    },
+    {
+      subtitle:"사비톡톡",
+      subtitle2:"집에서도 사비로와"
+    },
+  ]
+  res.render("subCont1.ejs", {menuList:menuList, listData:listData});
+})
+
+app.get("/sub2",function(req,res){
+  res.render("subCont1_2.ejs");
+})
+
+app.get("/sub3",function(req,res){
+  res.render("subCont1_3.ejs");
 })
 
 
+// 문화재
+app.get("/sub3_1", function(req,res){
+   // 데이터세팅하기
+   const sub3Data = [
+    {
+        image:"/img/subCont3/img1.jpg",
+        title:"부여 정림사지 오층석탑",
+        subTitle:"국보 제9호(지정일 1962.12.30)"
+    },
+    {
+      image:"/img/subCont3/img2.jpg",
+      title:"백제 금동대향로",
+      subTitle:"국보 제287호(지정일 1996.5.30)"
+  },
+  {
+    image:"/img/subCont3/img3.jpg",
+    title:"부여 정림사지 석불여래좌상",
+    subTitle:"보물 제108호(지정일 1963.01.21)"
+  },
+  {
+    image:"/img/subCont3/img4.jpg",
+    title:"부여 관북리 유적",
+    subTitle:"사적 제428호 (지정일2001.02.01)"
+  },
+  {
+    image:"/img/subCont3/img5.jpg",
+    title:"부여 정림사지",
+    subTitle:"사적 제301호 (지정일1983.03.26)"
+  },
+  {
+    image:"/img/subCont3/img6.jpg",
+    title:"부여 부소산성",
+    subTitle:"사적 제5호(지정일 1963.01.21)"
+  },
+  {
+    image:"/img/subCont3/img7.jpg",
+    title:"부여 구드래 일원",
+    subTitle:"명승 제6호"
+  },
+  {
+    image:"/img/subCont3/img8.jpg",
+    title:"부여동헌",
+    subTitle:"시도유형문화재 제96호 (1982.03.03)"
+  },
+  {
+    image:"/img/subCont3/img9.jpg",
+    title:"부여향교",
+    subTitle:"시도기념물 제125호 (지정일1997.12.23)"
+  },
+  ]
+  const Num = Number(req.params.num);
+  res.render("subCont3.ejs", {sub3Data:sub3Data, Num:Num});
+})
+
+app.get("/sub3_1/detail/:num", function(req,res){
+  const Num = Number(req.params.num);
+  res.render("subCont3_detail.ejs",{Num:Num})
+})
+
+
+// 문화시시설
+app.get("/sub3_1_2", function(req,res){
+  // 데이터세팅하기
+  const sub3Data = [
+  {
+    image:"/img/subCont3/num1/img1.jpg",
+    title:"국립부여박물관",
+    subTitle:"1월1일, 매주 월요일"
+  },
+  {
+    image:"/img/subCont3/num1/img2.png",
+    title:"정림사지박물관",
+    subTitle:"매주 월요일"
+  },
+  {
+    image:"/img/subCont3/num1/img3.jpg",
+    title:"사비도성 가상체험관",
+    subTitle:"매주 월요일,1월 1일,설날,추석"
+  },
+  {
+    image:"/img/subCont3/num1/img4.jpg",
+    title:"신동엽문학관.생가",
+    subTitle:"매주 월요일,음력8월15일"
+  },
+  {
+    image:"/img/subCont3/num1/img5.jpg",
+    title:"부여문화원",
+    subTitle:""
+  },
+  {
+    image:"/img/subCont3/img9.jpg",
+    title:"부소갤러리",
+    subTitle:""
+  },
+ ]
+ const Num = Number(req.params.num);
+ res.render("subCont3_2.ejs", {sub3Data:sub3Data, Num:Num});
+})
+
+app.get("/sub3_1_2/detail/:num", function(req,res){
+ const Num = Number(req.params.num);
+ res.render("subCont3_2_detail.ejs",{Num:Num})
+})
